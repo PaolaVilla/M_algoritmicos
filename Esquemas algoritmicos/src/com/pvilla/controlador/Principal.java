eecb20ab6b59352cba9de550203d7fe51bfd45d7
Paolavilla <alexa123villa@hotmail.com>
2019-02-21 09:03:55 -0500

    Ejercicio 1

---
+++ Esquemas algoritmicos/src/com/pvilla/controlador/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pvilla.controlador;
+
+import com.pvilla.modelo.Pedido;
+
+/**
+ * @Instituto Rumiñahui
+ * @author Paola
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido(1, 3);
+        
+    }
+}
---
+++ Esquemas algoritmicos/src/com/pvilla/modelo/ResuelveHeuristico.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pvilla.modelo;
+
+import java.util.Arrays;
+
+/**
+ * @Instituto Rumiñahui
+ * @author Paola
+ */
+public class ResuelveHeuristico {
+    /**
+*	Heurístico que busca el mayor número de pedidos que es
+*	posible realizar sin superar los límites de contaminación.
+*	El criterio de selección utilizado consiste en ir eligiendo
+*	los pedidos de menor a mayor contaminación por día.
+*	No siempre encuentra la solución óptima.
+ 
+*	@param pedidos conjunto de pedidos a fabricar
+*	@return orden de realización de pedios encontrado
+*/
+public static Pedido[] resuelve(Pedido[] pedidos) { 
+    SolParcial sol = new SolParcial(pedidos);
+
+// ordena el array de pedidos de menor a mayor contaminación
+// por día 
+Arrays.sort(pedidos);
+
+// va metiendo pedios a la solución hasta que no se
+// puedan meter más 
+for(Pedido p: pedidos) {
+if (!sol.factible(p)) {
+// no se pueden seguir metiendo pedidos 
+return sol.ordenDeFabricación();
+}
+
+// añade el pedido y pasa al siguiente 
+sol.añadePedido(p);
+
+}
+// retorna el mejor orden de fabricación encontrado 
+return sol.ordenDeFabricación();
+}
+}
+
+    
+
+
